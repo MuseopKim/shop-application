002a452354f8fed345600cebc1bf0a56ffcf61b1
Museop <museopkim0214@gmail.com>
2025-02-15 17:05:53 +0900

    feat: minimum total price brand id method

--- src/main/java/commerce/shop/application/component/aggregation/model/BrandCategoryPriceAggregation.java
+++ src/main/java/commerce/shop/application/component/aggregation/model/BrandCategoryPriceAggregation.java
@@ -1,9 +1,6 @@
 package commerce.shop.application.component.aggregation.model;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BrandCategoryPriceAggregation {
@@ -20,12 +17,25 @@ public class BrandCategoryPriceAggregation {
                 .collect(Collectors.groupingBy(ProductPrice::brandId));
     }
 
-    public int minimumTotalPriceOf(long brandId) {
+    public int calculateMinimumTotalPriceOf(long brandId) {
         List<ProductPrice> brandCategoryPrices = this.brandCategoryPrices.getOrDefault(brandId, Collections.emptyList());
 
         return brandCategoryPrices
                 .stream()
                 .map(ProductPrice::price)
                 .reduce(0, Integer::sum);
+    }
+
+    public List<ProductPrice> minimumPricesOf(long brandId) {
+        return brandCategoryPrices.getOrDefault(brandId, Collections.emptyList());
+    }
+
+    public long minimumTotalPriceBrandId() {
+        return brandCategoryPrices.keySet()
+                .stream()
+                .sorted(Comparator.comparingInt(this::calculateMinimumTotalPriceOf))
+                .limit(1)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("No price founds"));
     }
 }
--- src/test/java/commerce/shop/application/component/aggregation/model/BrandCategoryPriceAggregationTest.java
+++ src/test/java/commerce/shop/application/component/aggregation/model/BrandCategoryPriceAggregationTest.java
@@ -2,11 +2,10 @@ package commerce.shop.application.component.aggregation.model;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
-import commerce.shop.application.component.aggregation.model.BrandCategoryPriceAggregation;
-import commerce.shop.application.component.aggregation.model.ProductPrice;
 import commerce.shop.domain.category.Category;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BrandCategoryPriceAggregationTest {
@@ -26,7 +25,7 @@ class BrandCategoryPriceAggregationTest {
         BrandCategoryPriceAggregation aggregation = new BrandCategoryPriceAggregation(prices);
 
         // when
-        int totalPrice = aggregation.minimumTotalPriceOf(1L);
+        int totalPrice = aggregation.calculateMinimumTotalPriceOf(1L);
 
         // then
         then(totalPrice).isEqualTo(18000); // 10000 + 5000 + 3000
@@ -38,9 +37,46 @@ class BrandCategoryPriceAggregationTest {
         BrandCategoryPriceAggregation aggregation = new BrandCategoryPriceAggregation(prices);
 
         // when
-        int totalPrice = aggregation.minimumTotalPriceOf(Long.MAX_VALUE);
+        int totalPrice = aggregation.calculateMinimumTotalPriceOf(Long.MAX_VALUE);
 
         // then
         then(totalPrice).isZero();
+    }
+
+    @DisplayName("최저 총액 브랜드 ID를 찾는다")
+    @Test
+    void findMinimumTotalPriceBrandId() {
+        // given
+        BrandCategoryPriceAggregation aggregation = new BrandCategoryPriceAggregation(prices);
+        // Brand 1 총액 : 18000, Brand 2 총액 : 21000
+        long expectedBrandId = 1L;
+
+        // when
+        long actualBrandId = aggregation.minimumTotalPriceBrandId();
+
+        // then
+        then(actualBrandId).isEqualTo(expectedBrandId);
+    }
+
+    @DisplayName("특정 브랜드의 모든 카테고리 최저 가격을 조회한다")
+    @Test
+    void findMinimumPricesOfBrand() {
+        // given
+        BrandCategoryPriceAggregation aggregation = new BrandCategoryPriceAggregation(prices);
+        long brandId = 1L;
+        List<ProductPrice> givenPrices = Arrays.asList(
+                new ProductPrice(1L, Category.TOP, 10000),
+                new ProductPrice(1L, Category.OUTER, 5000),
+                new ProductPrice(1L, Category.PANTS, 3000)
+        );
+
+        // when
+        List<ProductPrice> prices = aggregation.minimumPricesOf(brandId);
+
+        // then
+        then(prices)
+                .hasSameSizeAs(givenPrices)
+                .usingRecursiveComparison()
+                .isEqualTo(givenPrices);
     }
 }
